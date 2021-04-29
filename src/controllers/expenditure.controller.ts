cbf7643831cb41fe804f35b9197535670c2fbf9a
Moisés Junior <nem.moises@hotmail.com>
2021-04-29 00:39:29 -0300

    Criando controller de despesas

--- src/controllers/expenditure.controller.ts
+++ src/controllers/expenditure.controller.ts
@@ -1,38 +1,105 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { Expenditure } from '../infra/entities/expenditure.entity';
 import { ExpenditureService } from '../services/expenditure.service';
 
 @Controller('/expenditure')
-export class CourseController {
+export class ExpenditureController {
   constructor(private readonly expenditureService: ExpenditureService) {}
 
   @Post()
-  async createCourse(): Promise<string> {
-    return 'Teste';
+  async createExpenditure(
+    @Res()
+    response: Response,
+    @Body()
+    expenditure: Expenditure,
+  ) {
+    try {
+      const newExpenditure = await this.expenditureService.createExpenditure(
+        expenditure,
+      );
+
+      return response.status(HttpStatus.CREATED).send(newExpenditure);
+    } catch (error) {
+      console.log(error);
+      return response.status(HttpStatus.BAD_REQUEST).send(error);
+    }
   }
 
   @Put(':id')
-  async updateCourse(
+  async updateExpenditure(
+    @Res()
+    response: Response,
     @Param('id')
     id: string,
-  ): Promise<string> {
-    return id;
+    @Body()
+    expenditure: Expenditure,
+  ) {
+    try {
+      const updatedExpenditure = await this.expenditureService.editExpenditure(
+        expenditure,
+        id,
+      );
+
+      return response.status(HttpStatus.OK).send(updatedExpenditure);
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).send(error);
+    }
   }
 
   @Get()
-  async listCourses(): Promise<string> {
-    return '';
+  async listExpenditures(
+    @Res()
+    response: Response,
+  ) {
+    try {
+      const expenditures = await this.expenditureService.listExpenditure();
+
+      return response.status(HttpStatus.OK).json(expenditures);
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).send(error);
+    }
   }
 
   @Get(':id')
-  async listCourse(
+  async listExpenditure(
+    @Res()
+    response: Response,
     @Param('id')
     id: string,
-  ): Promise<string> {
-    return id;
+  ) {
+    try {
+      const expenditure = await this.expenditureService.listExpenditureById(id);
+
+      return response.status(HttpStatus.OK).json(expenditure);
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).send(error);
+    }
   }
 
   @Delete()
-  async deleteCourse(): Promise<void> {
-    return;
+  async deleteCourse(
+    @Res()
+    response: Response,
+    @Param('id')
+    id: string,
+  ) {
+    try {
+      const expenditure = await this.expenditureService.deleteExpenditure(id);
+
+      return response.status(HttpStatus.OK).json(expenditure);
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).send(error);
+    }
   }
 }
